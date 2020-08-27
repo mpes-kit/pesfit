b6ad99b97ec4c32ec919afd6f5873b7968147abc
RealPolitiX <xrpatrick@gmail.com>
2020-08-27 08:43:09 -0500

    Added plot_bandpath() to pesfit.fitter

--- pesfit/fitter.py
+++ pesfit/fitter.py
@@ -1,10 +1,34 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from . import lineshape as ls
 import matplotlib.pyplot as plt
 
 
-def fit_plot(fitres, x, plot_components=True, downsamp=1, **kwds):
+####################
+# Fitting routines #
+####################
+
+def init_generator():
+
+    pass
+
+
+def model_generator():
+
+    pass
+
+
+def random_shift():
+
+    pass
+
+
+##########################
+# Visualization routines #
+##########################
+
+def plot_fit_result(fitres, x, plot_components=True, downsamp=1, **kwds):
     """ Plot the fitting outcomes.
     """
     
@@ -20,3 +44,44 @@ def fit_plot(fitres, x, plot_components=True, downsamp=1, **kwds):
     
     plt.plot(x, fitres.best_fit, '-r')
     plt.plot(x[::downsamp], fitres.data[::downsamp], '.k')
+
+
+def plot_bandpath(paths, ksymbols, erange=[], evals=None, path_inds=[], koverline=True, klines=False, ret=False, **kwds):
+    """ Plot momentum-energy map from a segment of the band mapping data.
+    """
+    
+    fsize = kwds.pop('figsize', (10, 6))
+    f, ax = plt.subplots(figsize=fsize)
+    
+    maxind = paths.shape[1]
+    
+    try:
+        elo, ehi = erange
+    except:
+        elo, ehi = evals[0], evals[-1]
+    plt.imshow(pathDiagram[:450, :], cmap='Blues', aspect=9.3, extent=[0, maxind, elo, ehi], vmin=0, vmax=0.5)
+    
+    # Momentum high-symmetry point annotation
+    if koverline:
+        klabels = ['$\overline{' + ksb +'}$' for ksb in ksymbols]
+    else:
+        klabels = ['$' + ksb + '$' for ksb in ksymbols]
+    
+    ax.set_xticks(path_inds)
+    ax.set_xticklabels(klabels, fontsize=15)
+    
+    # Draw vertical lines to label momentum high-symmetry points
+    if len(path_inds) * klines:
+        for p in path_inds[:-1]:
+            ax.axvline(x=p, c='r', ls='--', lw=2, dashes=[4, 2])
+
+    ax.yaxis.set_major_locator(MultipleLocator(2))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    ax.set_ylabel('Energy (eV)', fontsize=15, rotation=-90, labelpad=20)
+    ax.tick_params(axis='x', length=0, pad=6)
+    ax.tick_params(which='both', axis='y', length=8, width=2, labelsize=15)
+    
+    if ret:
+        return f, ax
