0ddadb2e65318730b70390f8ce8d9a82055838a0
RealPolitiX <xrpatrick@gmail.com>
2020-08-27 08:44:36 -0500

    Updated version to 0.1.5

--- pesfit/__init__.py
+++ pesfit/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = ('0', '1', '0')
+__version_info__ = ('0', '1', '5')
 __version__ = '.'.join(__version_info__)
 
 from . import lineshape, fitter
