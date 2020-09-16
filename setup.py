6d8c38c4f4d16bcb1dbfbf4d77dd878f08b30b8d
RealPolitiX <xrpatrick@gmail.com>
2020-09-16 17:39:27 -0500

    Minor text update

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# get the dependencies and installs
+# Get the dependencies and installs
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
+# Construct dependency links for registered packages
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
