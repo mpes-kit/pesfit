93e2db49dc02f1a8b331ae2e0796cd880070b7d0
RealPolitiX <xrpatrick@gmail.com>
2020-09-25 15:58:06 -0500

    Update version to 0.5.0

--- pesfit/__init__.py
+++ pesfit/__init__.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = ('0', '4', '8')
+__version_info__ = ('0', '5', '0')
 __version__ = '.'.join(__version_info__)
 __author__ = 'R. Patrick Xian'
 
-from . import lineshape, fitter
+from . import lineshape, fitter, utils
