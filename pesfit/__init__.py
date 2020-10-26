63b02fa7c38233ea7fa9ccbe168527508aa4fe4d
RealPolitiX <xrpatrick@gmail.com>
2020-10-26 08:59:25 -0500

    Update the version to 0.5.5

--- pesfit/__init__.py
+++ pesfit/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = ('0', '5', '2')
+__version_info__ = ('0', '5', '5')
 __version__ = '.'.join(__version_info__)
 __author__ = 'R. Patrick Xian'
 
