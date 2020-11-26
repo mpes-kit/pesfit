dd000eb512f16833456facc038f23032c4589410
RealPolitiX <xrpatrick@gmail.com>
2020-11-26 08:59:30 -0600

    Hyperparameter tuning now possible for 2-band synthetic data

--- benchmarks/WSe2_Kpoint_recon_2.sh
+++ benchmarks/WSe2_Kpoint_recon_2.sh
@@ -5,14 +5,26 @@ NBAND=2
 NSPEC=900
 EOFS="0.22 0.26 0.30 0.34 0.38"
 NWORK=4
-CHUNKSIZES=200
+CHUNKSIZE=300
 FPATH="WSe2_Kpoint_recon_2.txt"
 > $FPATH
 
-# Tune the relative energy shift in initialization for optimizing reconstruction outcome (2 bands)
-for CS in $CHUNKSIZES
+# Tune the relative energy shift in initialization for 1st band
+echo "Tuning initial conditions for reconstructing band #1 ..."
+for EOF in $EOFS
 do
-    echo "total band = $NBAND, total spec = $NSPEC, chunk size = $CS" >> $FPATH
-    /cygdrive/c/ProgramData/Anaconda3/python ./01_WSe2_Kpoint.py -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CS -ofs=$EOFS >> $FPATH
+    SHFTS="$EOF 0"
+    echo "total band = $NBAND, current band = 1, total spec = $NSPEC, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
+    /cygdrive/c/ProgramData/Anaconda3/python ./01_WSe2_Kpoint.py -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS >> $FPATH
+    echo "" >> $FPATH
+done
+
+# Tune the relative energy shift in initialization for 2nd band
+echo "Tuning initial conditions for reconstructing band #2..."
+for EOF in $EOFS
+do
+    SHFTS="0.3 $EOF"
+    echo "total band = $NBAND, current band = 2, total spec = $NSPEC, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
+    /cygdrive/c/ProgramData/Anaconda3/python ./01_WSe2_Kpoint.py -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS >> $FPATH
     echo "" >> $FPATH
 done
