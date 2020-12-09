d85f37e31ab6fed7841d676de6b04f25ac552d8d
RealPolitiX <xrpatrick@gmail.com>
2020-12-09 02:44:04 -0600

    Updated hyperparameters for hsymline benchmarks

--- benchmarks/WSe2_hsymline_recon_08.sh
+++ benchmarks/WSe2_hsymline_recon_08.sh
@@ -4,8 +4,8 @@
 NBAND=8
 NSPEC=186
 # EOFS="0.30 0.35 0.40 0.45 0.50 0.55"
-NWORK=10
-CHUNKSIZE=30
+NWORK=50
+CHUNKSIZE=5
 PYTHONPATH="/cygdrive/c/ProgramData/Anaconda3/python"
 CODEPATH="./03_WSe2_hsymline.py"
 FPATH="WSe2_hsymline_recon_08.txt"
@@ -23,6 +23,7 @@ do
 done
 
 # Tune the relative energy shift in initialization for 6th band
+EOFS="-0.10 -0.05 0.00 0.05 0.10 0.15 0.20"
 echo "Tuning initial conditions for reconstructing band #6..."
 for EOF in $EOFS
 do
@@ -33,20 +34,22 @@ do
 done
 
 # Tune the relative energy shift in initialization for 7th band
+EOFS="-0.10 -0.05 0.00 0.05 0.10"
 echo "Tuning initial conditions for reconstructing band #7..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 $EOF 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 $EOF 0"
     echo "total band = $NBAND, current band = 7, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 8th band
+EOFS="-0.10 -0.05 0.00 0.05 0.10"
 echo "Tuning initial conditions for reconstructing band #8..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 $EOF"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 $EOF"
     echo "total band = $NBAND, current band = 8, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
--- benchmarks/WSe2_hsymline_recon_14.sh
+++ benchmarks/WSe2_hsymline_recon_14.sh
@@ -4,68 +4,74 @@
 NBAND=14
 NSPEC=186
 # EOFS="0.30 0.35 0.40 0.45 0.50 0.55"
-NWORK=10
-CHUNKSIZE=30
+NWORK=80
+CHUNKSIZE=3
 PYTHONPATH="/cygdrive/c/ProgramData/Anaconda3/python"
 CODEPATH="./03_WSe2_hsymline.py"
 FPATH="WSe2_hsymline_recon_14.txt"
 > $FPATH # Will wipe the previous namesake txt file if it exists
 
 # Tune the relative energy shift in initialization for 9th band
+EOFS="0.10 0.15 0.20 0.25 0.30"
 echo "Tuning initial conditions for reconstructing band #9 ..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 $EOF 0 0 0 0 0"
     echo "total band = $NBAND, current band = 9, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 10th band
+EOFS="0.10 0.15 0.20 0.25 0.30"
 echo "Tuning initial conditions for reconstructing band #10..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 0.2 $EOF 0 0 0 0"
     echo "total band = $NBAND, current band = 10, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 11th band
+EOFS="0.10 0.15 0.20 0.25 0.30"
 echo "Tuning initial conditions for reconstructing band #11..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 0.2 0.25 $EOF 0 0 0"
     echo "total band = $NBAND, current band = 11, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 12th band
+EOFS="0.05 0.10 0.15 0.20 0.25 0.30 0.35"
 echo "Tuning initial conditions for reconstructing band #12..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 0.2 0.25 0.25 $EOF 0 0"
     echo "total band = $NBAND, current band = 12, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 13th band
+EOFS="0.15 0.20 0.25 0.30 0.35"
 echo "Tuning initial conditions for reconstructing band #13..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 0.2 0.25 0.25 0.30 $EOF 0"
     echo "total band = $NBAND, current band = 13, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
 done
 
 # Tune the relative energy shift in initialization for 14th band
+EOFS="-0.05 0.0 0.05 0.10 0.15"
 echo "Tuning initial conditions for reconstructing band #14..."
 for EOF in $EOFS
 do
-    SHFTS="0.40 0.35 0.25 0.05 0.1 0 0 0 $EOF 0 0 0 0 0"
+    SHFTS="0.40 0.35 0.25 0.05 0.1 -0.05 0.05 -0.05 0.2 0.25 0.25 0.30 0.30 $EOF"
     echo "total band = $NBAND, current band = 14, total spec = $NSPEC, worker = $NWORK, chunk size = $CHUNKSIZE, energy shift = $SHFTS" >> $FPATH
     $PYTHONPATH $CODEPATH -nb=$NBAND -ns=$NSPEC -op="parallel" -ds='LDA_synth_14' -bk="async" -nw=$NWORK -cs=$CHUNKSIZE -ofs $SHFTS -varin="theory" >> $FPATH
     echo "" >> $FPATH
